b2ead1ea4a8a965ea79a596089c6c744bcdaada2
Fer-50597 <fernandapirespinheiro@gmail.com>
2025-11-25 13:59:33 -0300

    01

--- PROVA/007/007.js
+++
@@ -1,8 +0,0 @@
-function somar() {
-    let valor_1 = Number(document.getElementById('valor_1').value)
-    let valor_2 = Number(document.getElementById('valor_2').value)
-    let soma = valor_1 + valor_2
-
-    document.getElementById('resultado').textContent = "RESULTADO: " + soma
-
-}
