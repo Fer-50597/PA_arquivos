01565e47beee38b2a29d6ebac484d23ecb681c2b
Fer-50597 <fernandapirespinheiro@gmail.com>
2026-02-06 15:40:30 -0300

    update

--- TRABALHO/trabalho.php
+++ TRABALHO/trabalho.php
@@ -1,25 +1,23 @@
 <?php
 
 //Classe Produto
-class Produto{
-    private $nome;
+abstract class Produto{
+    public $nome;
     public $preco;
-    private $quant_estoque;
     private $estado_pedido;
 
-    function __construct(string $nome, float $preco, int $quant_estoque, string $estado_pedido) 
+    function __construct(string $nome, float $preco, string $estado_pedido) 
     {
         $this->nome = $nome;
         $this->preco = $preco;
-        $this->quant_estoque = $quant_estoque;
         $this->estado_pedido = $estado_pedido;
     }
 
     public function info(): void
     {
+        echo "--Informações sobre o produto--<br>";
         echo "Nome do produto: {$this->nome}<br>";
         echo "Preço do produto: {$this->preco}<br>";
-        echo "Quantidade em estoque: {$this->quant_estoque}<br>";
         echo "Estado do pedido: {$this->estado_pedido}<br>";
     }    
 }
@@ -29,7 +27,7 @@ class Produto{
     protected string $nome;
     protected int $cpf;
     protected string $senha;
-    protected int $quantidade = 0;
+    public int $quantidade = 0;
 
 
     public function __construct(string $nome, int $cpf, string $senha, int $quantidade)
@@ -37,61 +35,128 @@ class Produto{
         $this->nome = $nome;
         $this->cpf = $cpf;
         $this->senha = $senha;
-        $this->quantidade =$quantidade;
+        $this->quantidade = $quantidade;
     }
 
 //---------------------------------------------------------------
-    abstract public function tipo_cliente(): string;
+    abstract public function tipo_cliente(): void;    
+    
+//---------------------------------------------------------------
+    abstract public function calcularValorTotal($quantidade, $preco): float;
+
+//---------------------------------------------------------------
+    abstract public function aplicarDesconto($valorTotal, $porcetagem): float;
+
+//---------------------------------------------------------------
+    abstract public function valor_Desconto($valorTotal, $valorCDesconto): float;
 
 //---------------------------------------------------------------
     public function compra(): void
     {
         echo "{$this->nome} realizou uma compra<br>";
+        echo "Quantidade comprada: {$this->quantidade}<br>";
     }
 
 //---------------------------------------------------------------
     public function info(): void
     {
+        echo "--Informações do cliente--<br>";
         echo "Nome do cliente: {$this->nome}<br>";
         echo "CPF do cliente: {$this->cpf}<br>";
     }
-
-   }
+}
 
 //Tipos de cliente ---------------------------------------------------------------
-   class ClienteComum extends Cliente{
 
-    public function tipo_cliente(): string{
-        return "Cliente Comum";
+   class ClienteComum extends Cliente{
+    
+    public function tipo_cliente(): void{
+        echo "---Cliente Comum---<br> ";
+        echo "---Desconto: 0%---<br>";
+    }
+    
+    public function calcularValorTotal($preco, $quantidade): float {
+        $valorTotal = $preco * $quantidade;
+        return $valorTotal;
+    }
+
+    public function aplicarDesconto($valorTotal, $desconto): float {
+        $valorCDesconto = $valorTotal * 0.00;
+        return $valorCDesconto;
+    }
+
+    public function valor_Desconto($valorTotal, $valorCDesconto): float {
+        $valor_Desconto = $valorTotal - $valorCDesconto;
+        return $valor_Desconto;
     }
 
    }
 
    class ClientePremium extends Cliente{
 
-    public function tipo_cliente(): string{
-        return "Cliente Premium";
+    public function tipo_cliente(): void{
+        echo "---Cliente Premium---<br>";
+        echo "---Desconto: 15%---<br>";
     }
 
-    public function aplicarDesconto(): void {
+    public function calcularValorTotal($preco, $quantidade): float {
+        $valorTotal = $preco * $quantidade;
+        return $valorTotal;
+    }
 
+    public function aplicarDesconto($valorTotal, $desconto): float {
+        $valorCDesconto = $valorTotal * 0.15;
+        return $valorCDesconto;
+    }
+
+    public function valor_Desconto($valorTotal, $valorCDesconto): float {
+        $valor_Desconto = $valorTotal - $valorCDesconto;
+        return $valor_Desconto;
 
    }
 }
+
+
 //---------------------------------------------------------------
     $produtoA = new Produto("Produto A", 2.99, 45, "Aberto");
     $produtoB = new Produto("Produto B", 100.00, 50, "Enviado");
+    $produtoC = new Produto("Produto C", 49.99, 45, "Pago");
+    $produtoD = new Produto("Produto D", 15.99, 45, "Cancelado");
 
     $clienteA = new ClienteComum("ClienteA", 110011100111, "12345", 10);
     $clienteB = new ClientePremium("ClienteB", 100011100111, "12345", 10);
+    $clienteC = new ClienteComum("ClienteC", 110010100111, "12345", 1);
+    $clienteD = new ClientePremium("ClienteD", 110011100101, "12345", 5);
+
 
     $clienteA->tipo_cliente();
     $clienteA->compra();
+    $clienteA->calcularValorTotal($preco, $quantidade);
     $clienteA->info();
     $produtoA->info();
+
     echo "<br>";
+
     $clienteB->tipo_cliente();
     $clienteB->compra();
+    $clienteB->calcularValorTotal($preco, $quantidade);
     $clienteB->info();
     $produtoB->info();
+    
+    echo "<br>";
+
+    $clienteC->tipo_cliente();
+    $clienteC->compra();
+    $clienteC->calcularValorTotal($preco, $quantidade);
+    $clienteC->info();
+    $produtoC->info();
+
+    echo "<br>";
+
+    $clienteD->tipo_cliente();
+    $clienteD->compra();
+    $clienteD->calcularValorTotal($preco, $quantidade);
+    $clienteD->info();
+    $produtoD->info();
+
 ?>
